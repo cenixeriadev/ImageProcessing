37e7a2fbc2c380259d1cb6021b4b836701badae7
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-05-31 18:53:13 -0500

    feat: implement image processing and storage functions for MinIO integration

---
+++ backend/worker_service/worker/image_processor.py
@@ -0,0 +1,60 @@
+import io
+import uuid
+from PIL import Image, ImageOps
+from worker.storage import download_image, upload_image
+import logging
+logger = logging.getLogger(__name__)
+
+def get_key(url : str)->str:
+    """
+    Extrae la clave del objeto de una URL de MinIO.
+    """
+    return url.split('/')[-2] + '/' + url.split('/')[-1]
+    
+
+def process_image_task(original_key: str, transformation: dict) -> str:
+    """
+    Procesa una imagen aplicando transformaciones y la sube a MinIO.
+    Devuelve el nuevo image_path.
+    """
+
+    #  Descargar imagen desde MinIO como objeto PIL
+    key = get_key(original_key)
+    logger.info(f"📥 Descargando imagen desde MinIO con key: {key}")
+    image = download_image(key)
+
+    #  Aplicar transformaciones
+    if transformation.get("resize"):
+        size = transformation["resize"]
+        width = size.get("width")
+        height = size.get("height")
+        if width and height:
+            logger.info(f"🔄 Redimensionando imagen a {width}x{height}")
+            image = image.resize((width, height))
+        else:
+            logger.warning("⚠️ Redimensionamiento solicitado sin ancho o alto especificado.")
+
+    if transformation.get("grayscale"):
+        logger.info(f"🌈 Aplicando transformación a escala de grises  con {transformation.get('grayscale')}")
+        image = ImageOps.grayscale(image)
+
+    # TODO: Añadir más implementaciones
+
+    # Guardar en memoria como bytes
+    buffer = io.BytesIO()
+    ext = original_key.split('.')[-1].upper()
+    format = {
+        "jpg": "JPEG",
+        "jpeg": "JPEG",
+        "png": "PNG",
+        "webp": "WEBP"
+    }.get(ext, "JPEG")
+    
+    image.save(buffer, format=format)
+    buffer.seek(0)
+
+    # Generar nuevo path y subir
+    new_key = f"transformed/{uuid.uuid4()}.{format.lower()}"
+    upload_image(buffer.read(), new_key, content_type=f"image/{format.lower()}")
+
+    return new_key
---
+++ backend/worker_service/worker/storage.py
@@ -0,0 +1,30 @@
+import boto3
+import os
+from PIL import Image
+import io
+from botocore.exceptions import ClientError
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID" , "minioadmin"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY" , "minioadmin"),
+    endpoint_url="http://minio:9000",  # Usar MinIO
+    region_name=os.getenv("AWS_REGION", "us-east-1")
+)
+
+BUCKET_NAME = os.getenv("BUCKET_NAME", "dev-bucket")
+
+def download_image(key: str) -> Image.Image:
+    try:
+        obj = s3.get_object(Bucket=BUCKET_NAME, Key=key)
+        return Image.open(io.BytesIO(obj["Body"].read()))
+    except ClientError as e:
+        raise Exception(f"❌ Error al descargar {key}: {e.response['Error']['Message']}")
+
+def upload_image(file_bytes: bytes, key: str, content_type: str = "image/jpeg")->None:
+    s3.put_object(
+        Bucket=BUCKET_NAME,
+        Key=key,
+        Body=file_bytes,
+        ContentType=content_type
+    )
