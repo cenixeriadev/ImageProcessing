7ccec04a10a651be76be575f53744ebbc82d9fe2
Anthony ramos roncal <codeartprogrammer@gmail.com>
2025-05-31 18:52:10 -0500

    feat: add image upload, transformation request, and retrieval endpoints

---
+++ backend/api_service/app/routes/images.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, UploadFile, File, Depends
+from app.services import storage, kafka_producer
+from app.models import ImageTask
+from app.database import SessionLocal
+import uuid
+from app.auth import get_current_user
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/images")
+async def upload_image(file: UploadFile = File(...), db: Session = Depends(get_db), user = Depends(get_current_user)):
+    content = await file.read()
+    s3_url = storage.upload_image(content, file.filename)
+
+    image = ImageTask(
+        user_id=user.id,
+        image_path=s3_url,
+        status="success",
+        transformation={
+            "filename": file.filename,
+            "content_type": file.content_type,
+            "size": len(content)
+        }
+    )
+
+    db.add(image)
+    db.commit()
+    db.refresh(image)
+
+    return {"id": str(image.id), "url": image.image_path}
+
+@router.post("/images/{image_id}/transform")
+def request_transformation(image_id: str, transformations: dict, db: Session = Depends(get_db), user = Depends(get_current_user)):
+    image = db.query(ImageTask).filter(ImageTask.id == image_id, ImageTask.user_id == user.id).first()
+    if not image:
+        raise HTTPException(status_code=404, detail="Imagen no encontrada")
+
+    # Extraer la key de la imagen desde la URL
+    s3_key = image.image_path.split('/')[-2] + '/' + image.image_path.split('/')[-1]
+
+    # Armamos el mensaje separado
+    message = {
+        "image_path": s3_key,
+        "user_id": user.id,
+        "task_id": image.id,
+        "transformation": transformations
+    }
+
+    kafka_producer.send_transformation_task(message)
+
+    return {"message": "Transformación solicitada, se procesará en background."}
+
+
+@router.get("/images/{image_id}")
+def get_image(image_id: str, db: Session = Depends(get_db), user = Depends(get_current_user)):
+    image = db.query(ImageTask).filter(ImageTask.id == image_id, ImageTask.user_id == user.id).first()
+
+    if not image:
+        raise HTTPException(status_code=404, detail="Imagen no encontrada")
+
+    return{
+        "id": str(image.id),
+        "url": image.image_path
+    }
